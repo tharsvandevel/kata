ec68ab39c1742a79b60bc073759cb4ba7159c09a
tharsvandevel <r0933637@student.thomasmore.be>
2023-05-05 12:12:42 +0200

    cleanup

--- src/Main.java
+++ src/Main.java
@@ -10,14 +10,14 @@ public class Main {
         for (int i = 0; i < letters.length; i++) {
             System.out.println(letters[i]);
         }
-        if (letters.length<=8){
-            System.out.println("“Password must be at least 8 characters”");
-        }
-        else {
-            System.out.println(password +" is valid");
+        if (letters.length <= 8) {
+            System.out.println("Password must be at least 8 characters");
+        } else {
+            System.out.println(password + " is valid");
         }
         //ik heb de rest niet kunnen laten werken dus heb ik het verwijdert
     }
+
     public static void main(String[] args) {
         Main main = new Main();
         main.checkPassword();
